bf013e415a28fef10d4386d48a38af0c1d91e3da
Joshua Koshy <89991793+josh-koshy@users.noreply.github.com>
2022-12-31 15:30:36 -0500

    Initial Commit

---
+++ CrospV4/CrospV4App.swift
@@ -0,0 +1,17 @@
+//
+//  CrospV4App.swift
+//  CrospV4
+//
+//  Created by Joshua Koshy on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CrospV4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
